60712a14bec60547a47321564af97a8d72dd9015
Milan Pandurov <milanpandurov@gmail.com>
2014-12-27 19:25:03 +0100

    Fixed crosscompile toolchain issues.

--- Toolchain-ARM.cmake
+++ Toolchain-ARM.cmake
@@ -1,13 +1,13 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
-SET(CMAKE_C_COMPILER arm-bcm2708-linux-gnueabi-gcc)
-SET(CMAKE_CXX_COMPILER arm-bcm2708-linux-gnueabi-g++)
+SET(CMAKE_C_COMPILER /home/milan/tools/rpi_crosscompile/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER /home/milan/tools/rpi_crosscompile/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-g++)
 
-SET(CMAKE_FIND_ROOT_PATH /home/milan/tools/rpi_crosscompile/arm-bcm2708/arm-bcm2708-linux-gnueabi/bin)
+SET(CMAKE_FIND_ROOT_PATH /home/milan/tools/rpi_crosscompile/rootfs)
 
-SET(TOOLCHAIN_INCLUDE /home/milan/tools/rpi_crosscompile/rootfs/usr/include)
-SET(TOOLCHAIN_LIB /home/milan/tools/rpi_crosscompile/rootfs/usr/lib)
+#SET(TOOLCHAIN_INCLUDE /home/milan/tools/rpi_crosscompile/rootfs/usr/include)
+#SET(TOOLCHAIN_LIB /home/milan/tools/rpi_crosscompile/rootfs/usr/lib)
 
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
