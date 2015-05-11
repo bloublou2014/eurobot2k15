6015f7ec41e1cd382dfe63b7862dfb4aea64e4bc
to92me <to92me@gmail.com>
2015-05-12 01:51:54 +0200

    Merged enemydetectorcommand

--- src/executors/msg/EnemyDetectorCommand.h
+++ src/executors/msg/EnemyDetectorCommand.h
@@ -4,6 +4,20 @@
 #include "messages/Command.h"
 #include "messages/CommandResponse.h"
 #include "utils/helper/BeaconData.h"
+
+
+//Milanovo begin
+#include "messages/Command.h"
+#include "messages/CommandResponse.h"
+#include "utils/helper/MotionState.h"
+#include "drivers/motion/MotionDriver.h"
+using namespace robot;
+using geometry::Point2D;
+using namespace v8;
+using javascript::JavaScriptMessageProvider;
+using javascript::ObjectWrap;
+//milanovo end
+
 using namespace modbus;
 
 namespace robot{
@@ -54,6 +68,28 @@ private:
     int maliCordY;
 };
 
-
 } // end robot namespace
+
+namespace motion{
+class AddStaticObject : public Command{ // done
+public:
+    static string NAME;
+    /* Exports object */
+    static void Init(Handle<Object> exports);
+    /* Constructor */
+    static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
+
+    AddStaticObject(vector<geometry::Point2D>& _edges):
+        Command(NAME,"MotionExecutor"),edges(_edges){}
+    AddStaticObject(const AddStaticObject& ao):Command(ao),edges(ao.edges){}
+    vector<geometry::Point2D> getEdges() const {return edges;}
+
+    Message* clone(){
+        return new AddStaticObject(*this);
+    }
+private:
+    vector<Point2D> edges;
+};
+}
+
 #endif // ENEMYDETECTORCOMMAND_H
